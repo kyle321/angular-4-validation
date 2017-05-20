11bc0a06504a2984c90b7b5d9b37e3a29e2a68ae
kyle321 <myk2030@gmail.com>
2017-05-20 10:17:19 +0300

    form validation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  onSubmit(value:any){
+    console.log(value);
+  }
 }
